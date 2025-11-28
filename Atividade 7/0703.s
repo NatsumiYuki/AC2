af79bf7ac816240b7ecf30a448cf96b61544d283
Natsumi Yuki <natsumiyuuki.a@gmail.com>
2025-11-28 13:51:40 -0300

    rename

--- Atividade 7/0703.s
+++ Atividade 7/0703.s
@@ -13,10 +13,14 @@ main:
 
     la s2, vetorC
 
+# Contagem de posições do vetor 3
+
     li t3, 5
     li t4, 0
 
     lw t0, 0(s0)
     lw t1, 4(s0)
     lw t2, 8(s0)
-
+     
+    loop 
+        bzte
